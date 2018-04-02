fbf1f48a8743f45bc55083f30142a04a2e62f60d
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2018-04-02 19:18:02 +0530

    Add files via upload

--- Binary search tree.cpp
+++ Binary search tree.cpp
@@ -80,8 +80,87 @@ class btree
 	    display(temp->right);
 	    
 	}
-    
 	
+	varun* search(varun *current,int x)
+	{
+		if(current!=NULL)
+		{
+        if(current->data==x)
+        return current;
+        else if(current->data > x)
+        return search(current->left,x);
+		else
+		return search(current->right,x);
+	    }
+	    else
+	    {
+	    return NULL;
+	    }
+	}
+	
+	void deletenode(int key)
+	{   
+		varun* k;
+		k=search(root,key);
+		if(k==NULL)
+		{
+		    cout<<"KEY NOT FOUND";	
+		}
+		else
+		{
+		    if(k->left==NULL&&k->right==NULL)
+			{
+			    varun *temp;
+			    temp=k->parent;
+			    if(temp->data < k->data)
+			    {
+				temp->right=NULL;
+			    k->parent=NULL;
+			    }
+			    else if(temp->data > k->data)
+			    {
+			    temp->left=NULL;
+			    k->parent=NULL;	
+				}
+		    }
+		    
+		    else if(k->left==NULL)
+		    {
+		    	varun *temp;
+		    	temp=k->parent;
+		    	if(temp->data < k->data)
+		    	{
+		    	temp->right=k->right;
+		    	k->parent=NULL;
+		        }
+		        else if(temp->data > k->data)
+		        {
+		    	temp->left=k->right;
+		    	k->parent=NULL;
+		        }
+			}
+			
+			else if(k->right==NULL)
+			{
+				varun *temp;
+				temp=k->parent;
+				if(temp->data < k->data)
+				{
+				temp->right=k->left;
+				k->parent=NULL;
+			    }
+			    else if(temp->data > k->data)
+			    {
+			    temp->left=k->left;
+				k->parent=NULL;	
+				}
+			}
+			
+			else
+			
+			
+		}
+	}
 };
 
 int main()
@@ -90,6 +169,12 @@ int main()
 	a.insert(4);
 	a.insert(6);
 	a.insert(7);
+	a.insert(3);
+	a.insert(5);
 	a.insert(12);
+	a.insert(2);
+	a.search(a.root,7);
+	a.display(a.root);
+	a.deletenode(7);
 	a.display(a.root);
 }
