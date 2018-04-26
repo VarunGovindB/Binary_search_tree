ea2da2b89ea82d9ca755604fde36d9a3347be658
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2018-04-26 21:50:29 +0530

    Update Binary search tree.cpp

--- Binary search tree.cpp
+++ Binary search tree.cpp
@@ -16,91 +16,118 @@ struct varun
 };
 
 class btree
-{   
-    public:
-            int s=0;
-	varun *root;
-	btree()
-	 {
-	 	root=NULL;
-	 }
-	 
-	void insert(int x)
-	{
+{
+	public:
+		int s;
+        varun *root;
+        btree()
+        {
+        	
+        	root=NULL;
+		}
+		
+		void insert(int x)
+		{
 		varun *temp = new varun;
 		varun *current = new varun;
 		temp->data=x;
 		temp->parent=temp->left=temp->right=NULL;
 		current = root;
+			if(root==NULL)
+			{
+				root=temp;
+			}
+			else
+			{
+			while(1)
+			{
+		    if(current->data>x)
+			{
+				if(current->left==NULL)
+				{
+					temp->parent=current;
+					current->left=temp;
+					break;
+				}
+				
+				else
+				{
+					current=current->left;
+				}
+			}
+			
+			else 
+			{
+				if(current->right==NULL)
+				{
+					temp->parent=current;
+					current->right=temp;
+					break;
+				}
+				
+				else
+				{
+					current=current->right;
+				}
+			}
+		    }
+		    }
+	
+		    
+		}
+	
+    	void display(varun *temp)
+		   {
+		    	if(temp==NULL)
+		    	return;
+				display(temp->left);
+				cout<<temp->data<<" ";
+				display(temp->right);
+		    	
+			}
 		
-	if(root==NULL)
-	{
-		root=temp;
-	}
-	else 
-	{
-	while(1)
-	{
-	if( current->data > x)
-	{   
-	    
-		if(current->left==NULL)
+		varun* search(varun *current, int x)
+		    {
+		    	if(current!=NULL)
+		    	{
+		    		if(current->data==x)
+		    		return current;
+		    		else if(current->data>x)
+		    		return search(current->left,x);
+		    		else
+		    		return search(current->right,x);
+				}
+				else
+				{
+					return NULL;
+				}
+			}
+			
+		void range(varun *temp, int k1, int k2)
 		{
-		    temp->parent=current;
-		    current->left=temp;
-		    break;
-		} 
-		else 
-		{
-			current= current->left;
+			if(temp==NULL)
+			return;
+			range(temp->left,k1,k2);
+			if(temp->data>=k1 && temp->data<=k2)
+		    {
+		    cout<<temp->data<<" ";
+		    }
+		    range(temp->right,k1,k2);
 		}
-	}
-	else
-	{
-		if(current->right==NULL)
+		
+		int count(varun *temp)
 		{
-		    temp->parent=current;
-		    current->right=temp;
-		    break;
-		} 
-		else 
-		{
-			current= current->right;
-		}
-	}
-	}
-	}
-	}
-	
-	void display(varun *temp)
-	{
-		if(temp==NULL)
-		return;
-		display(temp->left);
-		cout<<temp->data<<" ";
-	            display(temp->right);
-	    
-	}
-	
-	varun* search(varun *current,int x)
-	{
-		if(current!=NULL)
-		{
-        if(current->data==x)
-        return current;
-        else if(current->data > x)
-        return search(current->left,x);
-		else
-		return search(current->right,x);
+			if(temp==NULL)
+			return 0;
+			count(temp->left);
+			s++;
+			count(temp->right);
+			return s;
 	    }
-	    else
-	    {
-	    return NULL;
-	    }
-	}
-	
-	void deletenode(int key)
-	{   
+		
+		
+		void deletenode(int key)
+		{
 		varun* k;
 		k=search(root,key);
 		if(k==NULL)
@@ -109,82 +136,78 @@ class btree
 		}
 		else
 		{
-		    if(k->left==NULL&&k->right==NULL)
-			{
-			    varun *temp;
-			    temp=k->parent;
-			    if(temp->data < k->data)
-			    {
-				temp->right=NULL;
-			    k->parent=NULL;
-			    }
-			    else if(temp->data > k->data)
-			    {
-			    temp->left=NULL;
-			    k->parent=NULL;	
-				}
-		    }
-		    
-		    else if(k->left==NULL)
-		    {
-		    	varun *temp;
-		    	temp=k->parent;
-		    	if(temp->data < k->data)
-		    	{
-		    	temp->right=k->right;
-		    	k->parent=NULL;
-		        }
-		        else if(temp->data > k->data)
-		        {
-		    	temp->left=k->right;
-		    	k->parent=NULL;
-		        }
-			}
-			
-			else if(k->right==NULL)
+			if(k->left==NULL&&k->right==NULL)
 			{
 				varun *temp;
 				temp=k->parent;
-				if(temp->data < k->data)
+				if(temp->data>k->data)
 				{
-				temp->right=k->left;
-				k->parent=NULL;
-			    }
-			    else if(temp->data > k->data)
-			    {
-			    temp->left=k->left;
-				k->parent=NULL;	
+					temp->left=NULL;
+					k->parent=NULL;
+				}
+				else
+				{
+					temp->right=NULL;
+					k->parent=NULL;
 				}
 			}
 			
-			//else 
+			else if(k->left==NULL && k->right!=NULL)
+			{
+			    varun *temp;
+				temp=k->parent;
+				if(temp->data>k->data)
+				{
+					temp->left=k->right;
+					k->parent=NULL;
+				}
+				else
+				{
+					temp->right=k->right;
+					k->parent=NULL;
+				}
+			}
 			
+			else if(k->left!=NULL && k->right==NULL)
+			{
+			    varun *temp;
+				temp=k->parent;
+				if(temp->data>k->data)
+				{
+					temp->left=k->left;
+					k->parent=NULL;
+				}
+				else
+				{
+					temp->right=k->left;
+					k->parent=NULL;
+				}
+			}
 			
+	    	else
+	    	{
+			varun *temp;
+			varun *current;
+			temp=k->parent;
+			current=k->right;
+			while(current->left!=NULL)
+			{
+				current=current->left;
+			}
+			    if(temp->data>k->data)
+				{
+					temp->left=current;
+					current->parent=temp;
+				}
+				else
+				{
+					temp->right=current;
+					current->parent=temp;
+				}
+	        }
 		}
-	}
-	void range(varun *temp,int k1,int k2)
-	{
-		if(temp==NULL)
-		return;
-		range(temp->left,k1,k2);
-		if(temp->data>=k1 && temp->data<=k2)
-		{
-		cout<<temp->data<<" ";
-		}
-	            range(temp->right,k1,k2);
-	}
-	
-	
-            int count(varun *temp)
-	{           
-	
-		if(temp==NULL)
-		return 0;
-		count(temp->left);
-		s++;
-	            count(temp->right);
-	            return s;
-	}
+		}		
+			
 };
 
 int main()
@@ -203,7 +226,7 @@ int main()
 	a.s=0;
 	cout<<a.count(a.root);
 	cout<<endl;
-	a.deletenode(7);
+	a.deletenode(6);
 	a.display(a.root);
 	cout<<endl;
 	a.s=0;
@@ -213,4 +236,7 @@ int main()
 	cout<<endl;
 	a.s=0;
 	cout<<a.count(a.root);
+	a.deletenode(12);
+	cout<<endl;
+	a.display(a.root);
 }
